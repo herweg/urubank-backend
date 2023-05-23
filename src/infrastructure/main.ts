10329c96b42271e34e7b03f7b0e1ef4757f2de73
Diego Sánchez <clyzer.dev@gmail.com>
2023-05-23 18:50:39 -0300

    package.json change to use env variables

--- src/infrastructure/main.ts
+++ src/infrastructure/main.ts
@@ -1,9 +1,11 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { from } from 'rxjs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dotenv.config({ path: 'environments/.env.' + process.env.NODE_ENV });
   const app = await NestFactory.create(AppModule);
   const swagger = new DocumentBuilder()
     .setTitle('Urubank API')
