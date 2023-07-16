621b6190c6f2db77e62862d0fe22a0563ccdd540
Diego Sánchez <clyzer.dev@gmail.com>
2023-07-15 22:22:19 -0300

    cors removed

--- src/infrastructure/main.ts
+++ src/infrastructure/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   dotenv.config({ path: 'environments/.env.' + process.env.NODE_ENV });
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   const swagger = new DocumentBuilder()
     .setTitle('Urubank API')
